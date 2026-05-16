e52a42a177d17b6fbe8ea75c04c4a1e3db6f21d5
Nobodiiiii <mmu503690@gmail.com>
2026-05-16 16:29:27 +0800

    模型

--- src/main/java/com/nobodiiiii/createbiotech/content/cardboardbox/LargeCardboardBoxHandler.java
+++ src/main/java/com/nobodiiiii/createbiotech/content/cardboardbox/LargeCardboardBoxHandler.java
@@ -3,12 +3,15 @@ package com.nobodiiiii.createbiotech.content.cardboardbox;
 import com.nobodiiiii.createbiotech.CreateBiotech;
 import com.nobodiiiii.createbiotech.registry.CBItems;
 
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -17,6 +20,36 @@ import net.minecraftforge.fml.common.Mod;
 public class LargeCardboardBoxHandler {
 
 	private LargeCardboardBoxHandler() {}
+
+	@SubscribeEvent
+	public static void onEntityInteract(PlayerInteractEvent.EntityInteract event) {
+		Player player = event.getEntity();
+		if (!player.isCreative())
+			return;
+
+		InteractionHand hand = event.getHand();
+		ItemStack stack = player.getItemInHand(hand);
+		if (!stack.is(CBItems.LARGE_CARDBOARD_BOX.get()))
+			return;
+		if (player.isShiftKeyDown())
+			return;
+		if (CapturedEntityBoxItem.hasCapturedEntity(stack))
+			return;
+
+		Entity target = event.getTarget();
+		if (!(target instanceof Mob mobTarget))
+			return;
+
+		event.setCanceled(true);
+		event.setCancellationResult(InteractionResult.SUCCESS);
+
+		if (player.level().isClientSide())
+			return;
+		if (!CapturedEntityBoxHelper.captureEntity(stack, mobTarget))
+			return;
+
+		mobTarget.discard();
+	}
 
 	@SubscribeEvent
 	public static void onLivingDamage(LivingDamageEvent event) {
