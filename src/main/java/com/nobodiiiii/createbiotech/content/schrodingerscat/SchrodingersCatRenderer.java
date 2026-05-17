7d2dd033b8fa875d48e1a71e39108968beda67a0
Nobodiiiii <mmu503690@gmail.com>
2026-05-18 01:34:57 +0800

    猫

--- src/main/java/com/nobodiiiii/createbiotech/content/schrodingerscat/SchrodingersCatRenderer.java
+++ src/main/java/com/nobodiiiii/createbiotech/content/schrodingerscat/SchrodingersCatRenderer.java
@@ -2,22 +2,15 @@ package com.nobodiiiii.createbiotech.content.schrodingerscat;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.math.Axis;
 import com.nobodiiiii.createbiotech.CreateBiotech;
 import com.simibubi.create.foundation.blockEntity.renderer.SmartBlockEntityRenderer;
 
 import dev.engine_room.flywheel.lib.model.baked.PartialModel;
 import net.createmod.catnip.render.CachedBuffers;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
-import net.minecraft.client.renderer.entity.ItemRenderer;
-import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.Direction;
-import net.minecraft.world.item.ItemDisplayContext;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class SchrodingersCatRenderer extends SmartBlockEntityRenderer<SchrodingersCatBlockEntity> {
@@ -26,8 +19,6 @@ public class SchrodingersCatRenderer extends SmartBlockEntityRenderer<Schrodinge
 		PartialModel.of(CreateBiotech.asResource("block/schrodingers_cat/redstone_torch_off"));
 	private static final PartialModel TORCH_ON =
 		PartialModel.of(CreateBiotech.asResource("block/schrodingers_cat/redstone_torch_on"));
-	private static final ItemStack DISPLAY_SWORD = new ItemStack(Items.IRON_SWORD);
-	private static final float SWORD_SCALE = 0.5f;
 
 	public SchrodingersCatRenderer(BlockEntityRendererProvider.Context context) {
 		super(context);
@@ -44,7 +35,6 @@ public class SchrodingersCatRenderer extends SmartBlockEntityRenderer<Schrodinge
 
 		Direction facing = state.getValue(SchrodingersCatBlock.FACING);
 		renderTorch(blockEntity, poseStack, buffer, packedLight, state, facing);
-		renderSword(blockEntity, poseStack, buffer, packedLight, packedOverlay, facing);
 	}
 
 	private void renderTorch(SchrodingersCatBlockEntity blockEntity, PoseStack poseStack, MultiBufferSource buffer,
@@ -58,34 +48,6 @@ public class SchrodingersCatRenderer extends SmartBlockEntityRenderer<Schrodinge
 			.uncenter()
 			.light(packedLight)
 			.renderInto(poseStack, cutoutBuffer);
-	}
-
-	private void renderSword(SchrodingersCatBlockEntity blockEntity, PoseStack poseStack, MultiBufferSource buffer,
-		int packedLight, int packedOverlay, Direction facing) {
-		if (blockEntity.getLevel() == null)
-			return;
-
-		ItemRenderer itemRenderer = Minecraft.getInstance()
-			.getItemRenderer();
-		BakedModel bakedModel = itemRenderer.getModel(DISPLAY_SWORD, blockEntity.getLevel(), null, 0);
-
-		poseStack.pushPose();
-		poseStack.translate(0.5f, 10f / 16f, 0.5f);
-		poseStack.mulPose(Axis.YP.rotationDegrees(blockModelYRotation(facing)));
-		poseStack.mulPose(Axis.ZP.rotationDegrees(225));
-		poseStack.scale(SWORD_SCALE, SWORD_SCALE, SWORD_SCALE);
-		itemRenderer.render(DISPLAY_SWORD, ItemDisplayContext.NONE, false, poseStack, buffer, packedLight,
-			packedOverlay, bakedModel);
-		poseStack.popPose();
-	}
-
-	private static int blockModelYRotation(Direction facing) {
-		return switch (facing) {
-		case EAST -> 90;
-		case SOUTH -> 180;
-		case WEST -> 270;
-		default -> 0;
-		};
 	}
 
 	private static int torchModelYRotation(Direction facing) {
